8d31634e75c2518632407e074bb37cccba80c594
Park <tanagrit.sornchaiyagorn@gmail.com>
2023-12-01 13:55:36 +0700

    Add: add for loop in various type to ex1-6 files

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,15 @@
 // Exercise #3: Find a Minimum Number
 let numbers = [100, 20, 3, 1000];
 let minNumber;
-// Start coding here
+
+for (let i = 0; i < numbers.length; i++) {
+  if (!Number.isInteger(minNumber)) {
+    minNumber = numbers[i];
+  }
+
+  if (numbers[i] < minNumber) {
+    minNumber = numbers[i];
+  }
+}
 
 console.log(minNumber);
